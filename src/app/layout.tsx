074bb21b93a59cd4832625223817f3c200233a41
Arjun Sharma <arjunsharma6622@gmail.com>
2024-07-20 17:35:40 +0530

    added og and twitter imgs metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,32 @@ import Navbar from "@/components/Navbar";
 const p = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Not Done Yet",
-  description: "Building the LinkedIn for Sports World",
+  openGraph: {
+    title: 'Not Done Yet',
+    description: 'Building the LinkedIn for Sports World',
+    url: 'https://ndy-static.vercel.app',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dexnb3wk2/image/upload/v1721476623/ndy/vfynyiufjql3vpjlpbpd.png',
+        width: 1200,
+        height: 630,
+        alt: 'Not Done Yet',
+      },
+    ],
+    siteName: 'Not Done Yet',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Not Done Yet',
+    description: 'Building the LinkedIn for Sports World',
+    images: ['https://res.cloudinary.com/dexnb3wk2/image/upload/v1721476623/ndy/vfynyiufjql3vpjlpbpd.png'],
+  },
 };
+
+
+
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,30 +7,7 @@ import Projects from "./(components)/Projects";
 import Head from "next/head";
 import { Metadata } from 'next'; // if using TypeScript
 
-export const metadata: Metadata = {
-  openGraph: {
-    title: 'Not Done Yet',
-    description: 'Building the LinkedIn for Sports World',
-    url: 'https://res.cloudinary.com/dexnb3wk2/image/upload/v1721476623/ndy/vfynyiufjql3vpjlpbpd.png',
-    images: [
-      {
-        url: '/og_img.jpg',
-        width: 1200,
-        height: 630,
-        alt: 'Not Done Yet',
-      },
-    ],
-    siteName: 'Not Done Yet',
-    locale: 'en_US',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Not Done Yet',
-    description: 'Building the LinkedIn for Sports World',
-    images: ['https://res.cloudinary.com/dexnb3wk2/image/upload/v1721476623/ndy/vfynyiufjql3vpjlpbpd.png'],
-  },
-};
+
 
 const HomePage: React.FC = () => {
   return (
